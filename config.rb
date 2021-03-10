d213104ab24b18d02b838512d73edf0e1d765790
timwis <tim@timwis.com>
2021-03-10 17:46:37 +0000

    Build site to be served at /intranet I cannot seem to get the CSS and images to be served at relative URLs, despite the latter config settings

--- config.rb
+++ config.rb
@@ -27,34 +27,6 @@ activate :external_pipeline,
   source: '.tmp/dist',
   latency: 1
 
-# With alternative layout
-# page '/path/to/file.html', layout: 'other_layout'
-
-# Proxy pages
-# https://middlemanapp.com/advanced/dynamic-pages/
-
-# proxy(
-#   '/this-page-has-no-template.html',
-#   '/template-file.html',
-#   locals: {
-#     which_fake_page: 'Rendering a fake page with a local variable'
-#   },
-# )
-
-# Helpers
-# Methods defined in the helpers block are available in templates
-# https://middlemanapp.com/basics/helper-methods/
-
-# helpers do
-#   def some_helper
-#     'Helping'
-#   end
-# end
-
-# Build-specific configuration
-# https://middlemanapp.com/advanced/configuration/#environment-specific-settings
-
-# configure :build do
-#   activate :minify_css
-#   activate :minify_javascript
-# end
+config[:http_prefix] = '/intranet'
+config[:relative_assets] = true
+config[:relative_links] = true
