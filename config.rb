3744e4c822631837a658d8153a09cea247c92088
davidsauntson <david.sauntson@citizensadvice.org.uk>
2021-03-16 17:03:50 +0000

    feat: use proxies to avoid template duplication

--- config.rb
+++ config.rb
@@ -8,6 +8,12 @@ end
 # Layouts
 # https://middlemanapp.com/basics/layouts/
 
+# Proxies
+["home", "witness"].each do |page|
+  proxy "/#{page}.html", "index.html"
+end
+
+
 # Per-page layout changes
 page '/*.xml', layout: false
 page '/*.json', layout: false
@@ -17,6 +23,8 @@ config[:js_dir] = 'assets/javascripts'
 config[:css_dir] = 'assets/stylesheets'
 set :haml, { :format => :html5 }
 set :markdown_engine, :redcarpet
+set :directory_indexes, true
+activate :directory_indexes
 
 configure :build do 
   set :http_prefix, '/intranet'
