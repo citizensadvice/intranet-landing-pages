b5926802d1af2f898f4b0ae62f94cdc8ad050fb1
davidsauntson <david.sauntson@citizensadvice.org.uk>
2021-03-17 08:58:14 +0000

    test: add visual regression tests :tada:

---
+++ backstop.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  id: "backstop_default",
+  viewports: [{ label: "Large", width: 1024, height: 768 }],
+  scenarios: [
+    {
+      label: "Home page",
+      url: "http://localhost:4567/",
+      selector: ["document"],
+    },
+    {
+      label: "Witness page",
+      url: "http://localhost:4567/witness",
+      selector: ["document"],
+    },
+  ],
+  paths: {
+    bitmaps_reference: "backstop_data/bitmaps_reference",
+    bitmaps_test: "backstop_data/bitmaps_test",
+    html_report: "backstop_data/html_report",
+    ci_report: "backstop_data/ci_report",
+  },
+  report: ["browser"],
+  engine: "puppeteer",
+  engineOptions: {
+    args: ["--no-sandbox"],
+  },
+};
